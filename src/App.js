48fbba1a28a598b68a61f330e12c24c87d89c5ac
DWAYNE JONES <Dwaynekj@gmail.com>
2022-01-29 22:12:28 -0500

    Updated Contract Address and added Events

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-iimport React, { useEffect, useState } from "react";
+mport React, { useEffect, useState } from "react";
 import myEpicNft from './utils/MyEpicNFTv3.json';
 import './styles/App.css';
 import { ethers } from "ethers";
@@ -8,6 +8,7 @@ const TWITTER_HANDLE = 'senjoenyawd';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0xcc7b7BA884b01e2B6F3dBA705D306C8b9EC0Fdb9";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
@@ -28,6 +29,9 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account)
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener() 
     } else {
       console.log("No authorized account found")
     }
@@ -55,13 +59,44 @@ const App = () => {
       */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]); 
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener() 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error)
     }
   }
 
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0xed1b9614bF8125F93bE57a4679A4Ac03f2e51D40";
 
     try {
       const { ethereum } = window;
